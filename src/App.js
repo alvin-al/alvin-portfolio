de23b0219a0fe7e7b35635a1b6d223c0689afd14
Alvin Al <alvvxal@gmail.com>
2024-02-19 06:52:51 +0700

    Add hero banner section/main homepage content and style

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HeroBanner from './components/HeroBanner';
+
+function App() {
+  return (
+    <div className="App flex px-2 py-4 bg-[#171717] pb-48">
+      <HeroBanner />
+    </div>
+
+  );  
+}
+
+export default App;
---
+++ src/components/HeroBanner.js
@@ -0,0 +1,22 @@
+function HeroBanner() {
+    return(
+        <div id="heroBanner">
+            <div id="heroBanner-caption">
+                <p className="text-right pl-24 text-white text-sm">Hi, I am Alvin a web developer based in Bogor, Indonesia. Currently pursuing a degree in information systems at the Open University. I love to learn any field like design, tech, and web development. Feel free to explore my works and my thought</p>
+            </div>
+            <div id="heroBanner-name">
+                <h1 className="font-syne font-extrabold text-5xl text-left my-36 text-white">Alvin Al</h1>
+            </div>
+            <div id="navbar" className="pr-48">
+                <ul className="flex text-left gap-3 flex-col text-white">
+                    <li className="border-b-2 border-gray-100 pb-1 cursor-pointer">Work</li>
+                    <li className="border-b-2 border-gray-100 pb-1 cursor-pointer">Playground</li>
+                    <li className="border-b-2 border-gray-100 pb-1 cursor-pointer">Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+
+};
+
+export default HeroBanner;

5b0d38ca427a01821da083fc9b642a9136515b5b
Alvin Al <alvvxal@gmail.com>
2024-02-21 07:18:02 +0700

    Managing item on playground

--- src/components/PlaygroundSection.js
+++ src/components/PlaygroundSection.js
@@ -12,12 +12,21 @@ function PlaygroundSection (){
         <div id="playground">
             <h2 className="font-syne font-extrabold text-4xl text-left text-white border-b-2 pb-4">Playground</h2>
             <p className="text-left text-white text-base pt-4 mb-12">My journey as a web developer started from graphic design. I have a passion for creating beautiful and functional websites that communicate effectively with the target audience. I have learned UI design and web development skills to deliver high-quality websites.</p>
-            <div id="all-playground" className="grid grid-cols-2 gap-4">
+            <div id="all-playground" className="grid box-border gap-4">
                 <img src={Vlab} alt="" className="rounded-sm" />
-                <img src={CovidInstaPost} alt="" className="rounded-sm"/>
-                <img src={ErenStreetwear} alt="" className="rounded-sm"/>
-                <img src={DailydrivenBrosur} alt="" className="rounded-sm"/>
-                <img src={LaserPresisi} alt="" className="rounded-sm"/>
+                <div className="flex flex-row align-top gap-4 h-60 border">
+                    <div className="h-24 w-fit">
+                        <img src={CovidInstaPost} alt="" className="rounded-sm "/>
+                    </div>
+                    <div className="flex-auto">
+                    <img src={ErenStreetwear} alt="" className="rounded-sm "/>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+                <img src={DailydrivenBrosur} alt="" className="rounded-sm object-contain h-auto w-full"/>
+                <img src={LaserPresisi} alt="" className="rounded-sm object-contain h-auto w-full"/>
             </div>
         </div>
     );

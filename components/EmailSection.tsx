8c22c21bb3e550f4ca2e313cdde68c823944fc5b
alvvxal <alvvxal@gmail.com>
2024-09-19 02:43:44 +0000

    Add telegram button

--- components/EmailSection.tsx
+++ components/EmailSection.tsx
@@ -5,6 +5,8 @@ import gmail from "../public/icons/gmail.svg";
 import linkedin from "../public/icons/linkedin.svg";
 import Image from "next/image";
 import SubHeader from "./ui/SubHeader";
+import { BiLogoTelegram } from "react-icons/bi"; 
+import Link from "next/link";
 
 const emailDescription =
   "If you like my work or have any questions, feel free to contact me directly. I would love to hear from you and discuss your web development needs. You can reach me by email.";
@@ -19,21 +21,26 @@ const EmailSection = () => {
       <div>
         <EmailForm />
         <div className='flex flex-row justify-center md:justify-start gap-3 md:gap-6 lg:pl-[55%] xl:pl-[60%] md:px-52 md:items-start py-12 2xl:py-24'>
-          <a href='https://github.com/alvin-al'>
+        <Link href='https://t.me/alvinxal'>
+            <button className='hover:bg-slate-700 border border-white w-12 h-12 rounded-full flex justify-center items-center'>
+              <BiLogoTelegram className="w-6 h-6 fill-white"/>
+            </button>
+          </Link>
+          <Link href='https://github.com/alvin-al'>
             <button className='hover:bg-slate-700 border border-white w-12 h-12 rounded-full flex justify-center items-center'>
               <Image src={github} alt='github' />
             </button>
-          </a>
-          <a href='mailto:alvvxal@gmail.com'>
+          </Link>
+          <Link href='mailto:alvvxal@gmail.com'>
             <button className='hover:bg-slate-700 border border-white w-12 h-12 rounded-full flex justify-center items-center'>
               <Image src={gmail} alt='gmail' />
             </button>
-          </a>
-          <a href='https://www.linkedin.com/in/alvinxal/'>
+          </Link>
+          <Link href='https://www.linkedin.com/in/alvinxal/'>
             <button className='hover:bg-slate-700 border border-white w-12 h-12 rounded-full flex justify-center items-center'>
               <Image src={linkedin} alt='linkedin' />
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     </div>

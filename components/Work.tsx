1aaa2e23d940e7cd8e926368274bea05080495ae
alvin-al <alvvxal@gmail.com>
2024-11-10 21:07:08 +0700

    Add contentful items to work

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import WorkPictures from "./templates/WorkPictures";
 import urbia from "@/public/works/urbiaid.jpeg";
 import SubHeader from "./ui/SubHeader";
+import client from "@/utils/contentfulClient";
 
-function Work() {
+export default function Work() {
+  const [projects, setProjects] = React.useState([]);
+
+  useEffect(() => {
+    client.getEntries().then((response) => setProjects(response.items));
+  }, []);
+
+  console.log(projects);
+
   const p =
     "As a web developer, I create websites that facilitate communication between businesses and their customers or clients. Websites are powerful tools that can enhance brand awareness, generate leads, and increase conversions. I use the latest technologies and best practices to design and develop websites that are user-friendly and responsive.";
 
@@ -13,13 +23,16 @@ function Work() {
       <p className='text-left text-white text-base pt-4 pb-10 lg:pl-[55%]  xl:pl-[60%] xl:text-lg'>
         {p}
       </p>
-      <div id='all-works' className='w-full'>
-        <div className='lg:w-1/2'>
-          <WorkPictures src={urbia.src} title='Urbia ID' link={"urbia-id"} />
-        </div>
+      <div id='all-works' className='flex flex-col xl:flex-row gap-8'>
+        {projects.map((project: Project) => (
+          <WorkPictures
+            src={"http://" + project.fields.mainImage.fields.file.url}
+            title={project.fields.title}
+            link={project.fields.slug}
+            key={project.sys.id}
+          />
+        ))}
       </div>
     </div>
   );
 }
-
-export default Work;
--- components/templates/WorkPictures.tsx
+++ components/templates/WorkPictures.tsx
@@ -11,18 +11,11 @@ interface WorkPicturesProps {
 
 function WorkPictures({ src, title, link }: WorkPicturesProps) {
   return (
-    <Link href={`/products/${link}`}>
-      <div className='flex flex-col gap-2 rounded-md hover:scale-[98%] hover:opacity-90 transition ease-in-out delay-30 '>
-        <div className='w-full h-fit bg-yellow-50 cursor-pointer rounded-md hover:shadow-xl overflow-hidden'>
-          <Image
-            src={src}
-            alt={title}
-            className='w-full h-full object-cover '
-            width={1000}
-            height={630}
-          />
+    <Link href={`/projects/${link}`}>
+      <div className='flex flex-col gap-2 rounded-md hover:scale-[98%] hover:opacity-90 transition ease-in-out delay-30 h-full w-full'>
+        <div className='w-full h-fit bg-yellow-50 cursor-pointer rounded-md hover:shadow-xl overflow-hidden relative'>
+          <Image src={src} alt='' width={1000} height={800} objectFit='cover' />
         </div>
-
         <SubHeader2 classname='text-2xl'>{title}</SubHeader2>
       </div>
     </Link>

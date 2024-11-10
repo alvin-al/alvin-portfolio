474499f4596cfb2eee7b216a741baa68818a96c8
alvin-al <alvvxal@gmail.com>
2024-11-11 06:14:51 +0700

    Fix image

--- components/templates/WorkPictures.tsx
+++ components/templates/WorkPictures.tsx
@@ -14,7 +14,13 @@ function WorkPictures({ src, title, link }: WorkPicturesProps) {
     <Link href={`/projects/${link}`}>
       <div className='flex flex-col gap-2 rounded-md hover:scale-[98%] hover:opacity-90 transition ease-in-out delay-30 h-full w-full'>
         <div className='w-full h-fit bg-yellow-50 cursor-pointer rounded-md hover:shadow-xl overflow-hidden relative'>
-          <Image src={src} alt='' width={1000} height={800} objectFit='cover' />
+          <Image
+            src={src}
+            alt=''
+            width={1000}
+            height={800}
+            className='object-cover'
+          />
         </div>
         <SubHeader2 classname='text-2xl'>{title}</SubHeader2>
       </div>

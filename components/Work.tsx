77d6abfd734f0d5335f8a871f526457cf64e6e13
alvin-al <alvvxal@gmail.com>
2024-11-11 06:12:59 +0700

    Move get entries contentful

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,36 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import WorkPictures from "./templates/WorkPictures";
 import SubHeader from "./ui/SubHeader";
-import client from "@/utils/contentfulClient";
-
-interface Project {
-  sys: {
-    id: string;
-  };
-  fields: {
-    title: string;
-    slug: string;
-    mainImage: {
-      fields: {
-        file: {
-          url: string;
-        };
-      };
-    };
-  };
-}
+import GetEntriesContentful from "@/hooks/GetEntriesContentful";
 
 export default function Work() {
-  const [projects, setProjects] = useState<Project[]>([]);
-
-  useEffect(() => {
-    client.getEntries({ content_type: 'project' }).then((response) => {
-      setProjects(response.items as unknown as Project[]);
-    });
-  }, []);
-
-  console.log(projects);
+  const projects = GetEntriesContentful();
 
   const p =
     "As a web developer, I create websites that facilitate communication between businesses and their customers or clients. Websites are powerful tools that can enhance brand awareness, generate leads, and increase conversions. I use the latest technologies and best practices to design and develop websites that are user-friendly and responsive.";
@@ -42,12 +17,12 @@ export default function Work() {
         {p}
       </p>
       <div id='all-works' className='flex flex-col xl:flex-row gap-8'>
-        {projects.map((project: Project) => (
+        {projects.map((project) => (
           <WorkPictures
-            src={"http://" + project.fields.mainImage.fields.file.url}
-            title={project.fields.title}
-            link={project.fields.slug}
-            key={project.sys.id}
+            src={project.mainImage}
+            title={project.title}
+            link={project.slug}
+            key={project.id}
           />
         ))}
       </div>

d461965aefee460b12d8ffe6ab0e6456af08efcb
alvvxal <alvvxal@gmail.com>
2024-11-21 04:25:42 +0000

    Edit image size

--- components/templates/WorkPictures.tsx
+++ components/templates/WorkPictures.tsx
@@ -12,11 +12,11 @@ function WorkPictures({ src, title, link }: WorkPicturesProps) {
   return (
     <Link href={`${link}`}>
       <div className="flex flex-col gap-2 lg:gap-4 rounded-md h-full w-full">
-        <div className="w-full h-full cursor-pointer rounded-md hover:shadow-lg hover:delay-100 hover:transition hover:ease-in-out overflow-hidden relative">
+        <div className="w-full h-full cursor-pointer rounded-2xl hover:shadow-lg hover:duration-300 hover:transition hover:ease-in-out overflow-hidden relative">
           <Image
             src={src}
             alt="title"
-            width={1000}
+            width={800}
             height={800}
             className="object-cover h-full hover:scale-[110%] transition ease-in-out duration-300"
           />

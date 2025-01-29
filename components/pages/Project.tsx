f8d9f33183d102a58a174c858735195c46b91ccd
Alvin Al <alvvxal@gmail.com>
2025-01-29 13:16:42 +0700

    Add changes for homepage

--- components/pages/Project.tsx
+++ components/pages/Project.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import urbia from "@/public/works/urbiaid.jpeg";
 import Image from "next/image";
 import RoundedItem from "@/components/element/RoundedItem";
@@ -48,7 +48,7 @@ const Project = () => {
         <h1 className='text-2xl font-bold'>What is Lorem Ipsum?</h1>
         <p className={`leading-relaxed ${sourceSerif.className}`}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
@@ -68,7 +68,7 @@ const Project = () => {
           Latin words, consectetur, from a Lorem Ipsum passage, and going
           through the cites of the word in classical literature, discovered the
           undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and
           Evil) by Cicero, written in 45 BC. This book is a treatise on the
           theory of ethics, very popular during the Renaissance. The first line
           of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in

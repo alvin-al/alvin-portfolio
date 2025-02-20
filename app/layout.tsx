d30f859d05c1050a39a19e900da1c319a69359db
Alvin Al <alvvxal@gmail.com>
2025-02-20 11:48:20 +0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Navigation from "@/components/element/Navigation";
+import Script from "next/script";
 
 const PlusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -25,6 +26,18 @@ export default function RootLayout({
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='theme-color' content='#000000' />
         <meta name='My Portfolio' content='Alvin Al - Portfolio Web' />
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-N7V1X7SS9B'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-N7V1X7SS9B');
+  `}
+        </Script>
       </head>
       <body className={`${PlusJakarta.className} antialiased scroll-smooth`}>
         <Navigation />

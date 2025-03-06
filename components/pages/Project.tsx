86eb3735e72f14ac59e8ce3e3e5969214b77ff61
Alvin Al <alvvxal@gmail.com>
2025-03-06 21:47:03 +0700

    Add analytics

--- components/pages/Project.tsx
+++ components/pages/Project.tsx
@@ -1,19 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
 import ProjectHead from "../element/Project/ProjectHead";
 import ProjectText from "../element/Project/ProjectText";
 import ProjectMainImage from "../element/Project/ProjectMainImage";
 import ProjectViewButton from "../element/Project/ProjectViewButton";
-import contentfulClient from "@/lib/contentful";
-import { ProjectContentType } from "@/types";
+// // import contentfulClient from "@/lib/contentful";
+// import { ProjectContentType } from "@/types";
 
 const Project = () => {
   const path = usePathname().replace("/", "");
-  const [data, setData] = useState<ProjectContentType | null>(null);
+  // const [data, setData] = useState<ProjectContentType | null>(null);
   console.log(path);
 
-  console.log(data);
   return (
     <div className='px-8 md:px-64 flex flex-col mb-20'>
       {/* Title */}

c7bc79638f0e71c5ba9667b45ae1dcdedeaa4807
Alvin Al <alvvxal@gmail.com>
2025-12-06 21:15:13 +0700

    fix: edit home and blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-// import Work from "@/components/section/Work";
+import Work from "@/components/section/Work";
 import HeroBanner from "@/components/alt_page/HeroBanner";
 import React from "react";
 import AboutMeShort from "@/components/section/AboutMeShort";
 import Contact from "@/components/section/Contact";
-import Blog from "@/components/section/Blog";
+// import Blog from "@/components/section/Blog";
 
 const page = () => {
   return (
     <div className='bg-[#F8F0E5] w-full h-full pb-40 gap-48 md:gap-32 flex flex-col px-8 md:px-20'>
       <HeroBanner />
       <AboutMeShort />
-      {/* <Work /> */}
-      <Blog />
+      <Work />
+      {/* <Blog /> */}
       <Contact />
     </div>
   );
